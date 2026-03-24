8302dc9bf537001c55c4fc55e604d2ba58c2bb59
ShawnXuan <xiexuanx2@gmail.com>
2026-03-24 09:43:32 +0800

    benchmark test in test_special_perf

--- benchmark/test_special_perf.py
+++ benchmark/test_special_perf.py
@@ -1098,3 +1098,52 @@ def test_perf_lift_fresh_copy():
         dtypes=FLOAT_DTYPES,
     )
     bench.run()
+
+
+class UpsampleLinear1dBackwardBenchmark(Benchmark):
+    """
+    Benchmark for upsample_linear1d_backward operator.
+    """
+
+    def set_more_shapes(self):
+        shapes_3d = [(4, 16, 2**i) for i in range(4, 14, 2)]
+        shapes_2d = [(16, 2**i) for i in range(6, 16, 2)]
+        return shapes_3d + shapes_2d
+
+    def get_input_iter(self, cur_dtype):
+        for shape in self.shapes:
+            for scale_factor in [0.5, 2.0]:
+                for align_corners in [False, True]:
+                    in_w = shape[-1]
+                    out_w = max(1, int(in_w * scale_factor))
+
+                    grad_shape = list(shape)
+                    grad_shape[-1] = out_w
+
+                    grad = torch.randn(
+                        grad_shape,
+                        device=self.device,
+                        dtype=cur_dtype,
+                    )
+
+                    yield grad, None, list(shape), align_corners, [scale_factor]
+
+    def get_tflops(self, op, *args, **kwargs):
+        grad, output_size, input_size, align_corners, scale_factors = args
+        return grad.numel() * 2
+
+
+@pytest.mark.upsample_linear1d_backward
+@pytest.mark.parametrize(
+    "dtype",
+    FLOAT_DTYPES,
+)
+def test_upsample_linear1d_backward_perf(dtype):
+    bench = UpsampleLinear1dBackwardBenchmark(
+        op_name="upsample_linear1d_backward",
+        torch_op=flag_gems.ops.upsample_linear1d_backward,
+        dtypes=[dtype],
+    )
+
+    bench.run()
+
