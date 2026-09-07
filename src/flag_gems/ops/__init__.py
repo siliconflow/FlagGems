8caf203e336c5523245052de0953a5f8027ea25c
LinuxKiller <wangzixu13145687@gmail.com>
2026-09-07 15:29:14 +0800

    feat: add cross-backend int_mm and out support
    Signed-off-by: LinuxKiller <wangzixu13145687@gmail.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -543,6 +543,7 @@ from flag_gems.ops.index_put import _index_put_impl_, index_put, index_put_
 from flag_gems.ops.index_reduce import index_reduce, index_reduce_, index_reduce_out
 from flag_gems.ops.index_select import index_select
 from flag_gems.ops.index_select_backward import index_select_backward
+from flag_gems.ops.int_mm import int_mm, int_mm_out
 from flag_gems.ops.is_nonzero import is_nonzero
 from flag_gems.ops.isclose import allclose, isclose
 from flag_gems.ops.isfinite import isfinite
@@ -1620,6 +1621,8 @@ __all__ = [
     "index_reduce_out",
     "index_select",
     "index_select_backward",
+    "int_mm",
+    "int_mm_out",
     "is_nonzero",
     "isclose",
     "isfinite",
